0fc828e434a8ae47a448293cd7f3da8fb52cf949
Tom Rake <zzzap1957@gmail.com>
2023-05-22 09:44:41 -0400

    Update to use msys64/ucrt64 environment.

--- main.lisp
+++ main.lisp
@@ -1,14 +1,15 @@
 (defpackage #:config-sqlite3/main
   (:use
    #:cl
-   #:sqlite/config)
-  (:export
-   #:preload))
+   #:sqlite/config))
 
 ;;;; dll sqlite3-lib in system sqlite-ffi
 (in-package #:config-sqlite3/main)
 
-(defun preload ()
-  (define-sqlite-lib "c:/devel/msys64/mingw64/bin/libsqlite3-0.dll"))
 
-(preload)
+  #+ (and win32 64-BIT)
+  (define-sqlite-lib "c:/devel/msys64/ucrt64/bin/libsqlite3-0.dll")
+  #+ (and windows 32-BIT-HOST)
+  (define-sqlite-lib "c:/devel/external-libs/sqllite-341000/win-i386/sqlite3.dll")
+
+
